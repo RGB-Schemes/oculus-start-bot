62cccb8d2cfc5cf7957270f3eb2f1750fb17d058
Gerald McAlister <gemisis@users.noreply.github.com>
2020-05-20 18:03:20 -0700

    Setup for Production
    [Problem]
    Currently, the bot does not actually assign any roles!
    
    [Solution]
    Some final cleanups for the bot, and setup for assigning roles.
    
    [Testing]
    Tested and confirmed working.

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 ROLE_VALUE = os.getenv('ROLE_VALUE')
 bot = commands.Bot(command_prefix='!')
-existingVerifications = SqliteDict('./my_db.sqlite', autocommit=True)
+existingVerifications = SqliteDict('./verified_members.sqlite', autocommit=True)
 
 @bot.command(name='finishmygameforme')
 async def finishmygameforme(ctx):
@@ -37,25 +37,23 @@ async def verify(ctx, forumUsername: str):
     if forumUsername not in existingVerifications:
         if startParser.exists and startParser.isOculusStartMember:
             if str(startParser.discordUsername) == str(ctx.author):
-                """
-                TODO: Uncomment for production.
                 role = utils.get(ctx.guild.roles, name=ROLE_VALUE)
                 await ctx.message.author.add_roles(role)
-                """
                 existingVerifications[forumUsername] = str(ctx.author)
                 embed.add_field(name=":white_check_mark:", value="Confirmed that [{0}]({2}) is a member of Oculus Start! Assigning the role of {1}! If this does not work, please reach out to the moderators.".format(startParser.discordUsername, ROLE_VALUE, addr))
             else:
                 if startParser.discordUsername == None:
-                    embed.add_field(name=":x:", value="Couldn't find a comment from {0} with a Discord handle! Please comment on [your profile]({1}) with the **exact** Discord handle (it is case sensative!) See the below image for how this should look:".format(forumUsername, addr))
+                    embed.add_field(name=":x:", value="Couldn't find a comment from {0} with a Discord handle! Please comment on [your profile]({1}) with the **exact** Discord handle (it is case sensative!). See the below image for how this should look on your profile:".format(forumUsername, addr))
                     embed.set_image(url="https://cdn.discordapp.com/attachments/711883614266064986/712060083478986772/unknown.png")
+                    embed.add_field(name="Note", value="There may be caching delays if you have just commented, so please try this command again after a minute or two. Again, Discord names are case sensative, so make sure your capitalization is correct!")
                 else:
                     embed.add_field(name=":x:", value="Confirmed that [{0}]({3}) is a member of Oculus Start but their Discord username was listed as {2}, not {1}!".format(forumUsername, ctx.author, startParser.discordUsername, addr))
         else:
             if startParser.exists == False:
-                embed.add_field(name=":x:", value="{0} is **NOT** a valid Oculus username! Please enter a valid Oculus username as the input parameter! See this image for where to find your Oculus username:".format(forumUsername, addr))
+                embed.add_field(name=":x:", value="{0} is **NOT** a valid Oculus username! Please enter a valid Oculus username as the input parameter! See this image for where to find your Oculus username on your forum profile:".format(forumUsername, addr))
                 embed.set_image(url="https://media.discordapp.net/attachments/712041288836579368/712059524756013176/unknown.png")
             else:
-                embed.add_field(name=":x:", value="[{0}]({1}) is **NOT** a member of Oculus Start!".format(forumUsername, addr))
+                embed.add_field(name=":x:", value="[{0}]({1}) is not a member of Oculus Start according to their forum profile! You'll want to ensure that you have the **Oculus Start Member** role on your profile! This may take time to appear if you are new to Oculus Start, so please be patient.".format(forumUsername, addr))
     else:
         embed.add_field(name=":x:", value="[{0}]({2}) has already verified {1} as their Discord username! Please contact a moderator if this is incorrect!".format(forumUsername, existingVerifications[forumUsername], addr))
 
