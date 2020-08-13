2336e29d2241a69d49ad6d94a7f2a97e3af69422
Gerald McAlister <gemisis@users.noreply.github.com>
2020-08-12 19:07:08 -0700

    Fix Bot Startup Command

--- scripts/startbot.sh
+++ scripts/startbot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Starting bot...
-nohup python3 /home/ec2-user/src/bot.py > /home/ec2-user/bot.log &
+python3 -u /home/ec2-user/src/bot.py > /home/ec2-user/bot.log &
 echo Writing bot pid to file...
 echo $! > /home/ec2-user/bot.pid
